24c40a21a54ce7b64f08267c7c0f2c22a440ff73
Brian Luft <brian@lincolnloop.com>
2020-09-14 15:08:54 -0700

    List display with image thumbs for Venue admin

--- venues/admin.py
+++ venues/admin.py
@@ -1,4 +1,19 @@
 from django.contrib import admin
+from django.utils.html import format_html
+
 from .models import Venue
 
-admin.site.register(Venue)
+
+class VenueAdmin(admin.ModelAdmin):
+    def image_tag(self, obj):
+        if obj.image:
+            return format_html(
+                '<img width="250px" src="{}" />'.format(obj.image.url)
+            )
+        return ""
+
+    image_tag.short_description = "Image"
+    list_display = ("name", "image_tag", "description", "is_public")
+
+
+admin.site.register(Venue, VenueAdmin)
